94be86c1d923b3c88a8d9d5ee114ab4710b51972
vivinkv6 <vivinkv0@gmail.com>
2022-06-19 20:11:03 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import Home from './Pages/Home';
 import Navbars from "./Components/Navbar";
-import{BrowserRouter,Routes,Route} from 'react-router-dom';
-import NotFound from './Pages/NotFound';
+
 
 
 function App() {
 
   return (
-    <BrowserRouter>
-    <Navbars/>
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path='*' element={<NotFound/>}/>
-    </Routes>
-    </BrowserRouter>
+   <>
+   <Navbars/>
+      <Home />
+    </>
   );
 }
 
